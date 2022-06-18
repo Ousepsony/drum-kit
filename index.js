37046d4d6fe26dd904a456f27ac6969616ecbd5f
Ousepsony <100663601+Ousepsony@users.noreply.github.com>
2022-06-18 12:25:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 for(let i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
 }
-
-    document.addEventListener("keyup",pressed);
-
-function pressed(event) {
-    if(event.code==="KeyW"){
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyA"){
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyS"){
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyD"){
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyJ"){
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyK"){
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
-    }
-    else if(event.code==="KeyL"){
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
-    }
-};
-
-
-
-function clicked(){
-    console.log("sounds/"+this.name+".mp3");
+function clicked(event) {
     var audio = new Audio("sounds/"+this.name+".mp3");
     audio.play();
-}
+    this.style.animation ='effect'
+    // this.style.animation.play = 'effect';
+    
+    console.log(this)
+};
 
-// document.addEventListener('keydown', logKey);
-
-// function logKey(e) {
-//   log.textContent += ` ${e.code}`;
-// }
+window.addEventListener("keyup",function(event){
+    switch (event.key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            document.style.animation ='effect 1s'
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;           
+            
+        default:
+            break;
+    }
+})
